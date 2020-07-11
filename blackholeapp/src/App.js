c246597200d2496874305ddfc4ecd265fd73a8dc
BrandyBecker <brandybeckerfsw@gmail.com>
2020-07-11 13:06:37 -0400

    initial setup

---
+++ blackholeapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Nav from './nav/nav.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <p>The Blackhole Project</p>
+      <p>A place to vent about your emotions & feelings. Unspoken words that you'd usually bottle up - let it out here.
+        Each message sent to the void will self-destruct within 24 hours - *poof*
+      </p>
+      <p>a few submissions</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ blackholeapp/src/nav/nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './nav.css';
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <h1>nav</h1>
+      <a>home</a>
+      <a>about</a>
+      <a>archive</a>
+      <a>submit</a>
+    </div>
+  );
+}
+
+export default Nav;
